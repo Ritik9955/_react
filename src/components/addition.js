6e28dfbcf14905b01023cb4fc371fd4a61114f23
Ritik kumar gupta <42289891+Ritik9955@users.noreply.github.com>
2023-04-22 00:23:08 +0530

    first commit

---
+++ src/components/addition.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+ 
+
+function Adition() {
+    const [add ,setadd]=useState();
+
+    function Addition(a, b) {
+        // let c=a+b;
+        
+    }
+    // setadd(32);
+    Addition(23,5);
+    console.log(add);
+    return (
+       
+        <div>
+            <h1> i am add div</h1>
+        </div>
+    );
+}
+export default Adition;
